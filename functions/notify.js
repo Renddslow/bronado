662cce84359e9c2515b30fc0ba8b64e46d563f14
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-09-02 15:52:16 -0500

    feat: set up the invite form

--- functions/notify.js
+++ functions/notify.js
@@ -1,17 +1,17 @@
-const twilio = require('twilio');
+const twilio = require("twilio");
 
 const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
 exports.handler = async (event, context) => {
-    const { headcount, family } = JSON.parse(event.body);
+  const { firstName, lastName, email } = JSON.parse(event.body);
 
-    await client.messages.create({
-        body: `The ${family} family is coming to Rowan's party! They are bringing ${headcount} guests.`,
-        from: '+14023789202',
-        to: process.env.PHONE_NUMBER,
-    });
+  await client.messages.create({
+    body: `${firstName} ${lastName} (${email}) is planning to come nut-tap Mitchell.`,
+    from: "+14023789202",
+    to: process.env.PHONE_NUMBER,
+  });
 
-    return {
-        statusCode: 200,
-    };
-}
+  return {
+    statusCode: 200,
+  };
+};
